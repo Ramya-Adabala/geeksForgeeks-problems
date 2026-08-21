19012fd1fa73398da74ae70e88851cba46852f60
Ramya-Adabala <ramya.adabala1@gmail.com>
2026-08-21 16:28:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Balanced Array/balanced-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minValueToBalance(List<Integer> arr) {
+        // code here
+        int n=arr.size();
+        int sum1=0,sum2=0;
+        for(int i=0;i<n/2;i++){
+            sum1+=arr.get(i);
+        }
+        for(int i=n/2;i<n;i++){
+            sum2+=arr.get(i);
+        }
+        return Math.abs(sum1-sum2);
+    }
+};
